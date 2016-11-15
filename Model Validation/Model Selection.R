aee41c7f63abb23f9eed13f43a0565d3e9d1f299
Rizwaan Adil <riz.adil@gmail.com>
2016-11-15 13:23:03 +0530

    Added gitignore file

---
+++ Model Validation/Model Selection.R
@@ -0,0 +1,6 @@
+# Source : https://www.udemy.com/introduction-to-data-science/learn/v4/t/lecture/2369004
+
+
+
+
+setwd("C:/Users/adilr/OneDrive - Hewlett Packard Enterprise/Work/Learning/Data Science/data_science/Model Validation")
